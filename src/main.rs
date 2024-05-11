282c66f2d36e04bd84f0d8f4acce9e900d1f99eb
caojian <1736665642@qq.com>
2024-05-11 09:10:52 +0800

    Fix bugs.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
 mod fuzz;
+mod hunter_serach;
+mod zoomeye_search;
 use clap::Parser;
 use colored::Colorize;
+use tokio;
 
 #[derive(Parser)]// 定义命令行参数
-#[command(author="Caojiang", name = "SpringFuzz", version = "0.1.0", about, long_about = None)]// 设置作者、版本、介绍等
+#[command(author="Re1ax_Cyber", name = "SpringBoot-Rscan", version = "0.1.0", about=r"
+ __            _             _                 _          __
+/ _\_ __  _ __(_)_ __   __ _| |__   ___   ___ | |_       /__\___  ___ __ _ _ __
+\ \| '_ \| '__| | '_ \ / _` | '_ \ / _ \ / _ \| __|____ / \// __|/ __/ _` | '_ \
+_\ \ |_) | |  | | | | | (_| | |_) | (_) | (_) | ||_____/ _  \__ \ (_| (_| | | | |
+\__/ .__/|_|  |_|_| |_|\__, |_.__/ \___/ \___/ \__|    \/ \_/___/\___\__,_|_| |_|
+   |_|                 |___/
+                                                                     Author:Re1ax_Cyber.
+
+Springboot-Rscan——集自由调节并发/代理池/自定义cookie/多url为一身的SpringBoot扫描工具，使用Rust异步并发执行，效率大翻倍！
+")]// 设置作者、版本、介绍等
 struct Args {
     /// The URL to scrape
     #[clap(short='u', long="url", value_name = "URL",default_value = "")]// 设置url
@@ -23,10 +36,26 @@ struct Args {
     /// The sem to use
     #[clap(short='s', long="sem", value_name = "sem",default_value = "10")]// 定义cookie
     sem: String,
+    /// The sem to use ，spring or dir
+    #[clap(short='m', long="method", value_name = "method",default_value = "spring")]// 定义cookie
+    method: String,
 }
 
-use tokio;
 
+async fn process_url(url: &str) {
+    let args = Args::parse();//解析命令行参数
+    let fuzzer = fuzz::Fuzz::new(url, &*args.dict, &*args.proxy, &*args.cookies, &*args.sem);
+    match args.method.as_str() {
+        "spring" => {
+            fuzzer.check().await;
+            fuzzer.fuzz().await;
+        }
+        "dir" => fuzzer.fuzz().await,
+        _ => {
+            println!("{}", "method error".bright_yellow())
+        }
+    }
+}
 #[tokio::main]
 async fn main() {
     let args = Args::parse();//解析命令行参数
@@ -39,7 +68,7 @@ async fn main() {
 _\ \ |_) | |  | | | | | (_| | |_) | (_) | (_) | ||_____/ _  \__ \ (_| (_| | | | |
 \__/ .__/|_|  |_|_| |_|\__, |_.__/ \___/ \___/ \__|    \/ \_/___/\___\__,_|_| |_|
    |_|                 |___/
-                                                                     Author:Relax_Cyber.
+                                                                     Author:Re1ax_Cyber.
  ".green();
     println!("{}",logo);
     if  url_file != ""{//如果url文件不为空
@@ -47,16 +76,16 @@ _\ \ |_) | |  | | | | | (_| | |_) | (_) | (_) | ||_____/ _  \__ \ (_| (_| | | | 
         let urls = fuzz::Fuzz::read_urls(&url_file);
         //遍历url文件中的url
         for url in urls {
-            let fuzzer = fuzz::Fuzz::new(&url, &*args.dict,&*args.proxy,&*args.cookies,&*args.sem);//创建Fuzz对象
-            fuzzer.fuzz().await;//调用fuzz方法
+            //process_url(&*url).await;
+            tokio::join!(process_url(&*url));//并发执行
         }
 
-    } else if url != ""{//如果url不为空
-        //创建Fuzz对象
-        let fuzzer = fuzz::Fuzz::new(&url, &*args.dict,&*args.proxy,&*args.cookies,&*args.sem);//创建Fuzz对象
-        fuzzer.check().await;//调用fuzz方法
-        fuzzer.fuzz().await;//调用fuzz方法
+
+    } else if url != "" {
+        // 单独处理URL
+        process_url(url).await;
     } else {
-        eprintln!("{}", "请提供URL文件或单个URL".bright_yellow());//输出错误信息
+        eprintln!("{}", "请提供URL文件或单个URL".bright_yellow());
     }
+
 }
